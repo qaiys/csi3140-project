92c5fc23ceeda2423ed6abfa7ae20e4ce784a384
Salina Bee <siriuspilas@gmail.com>
2023-07-22 19:42:06 -0400

    Added account creation functionality

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,7 +1,7 @@
-var form, name, email, username, password, password2;
-var inputGroup;
+var form, name1, email, username, password, password2;
 var submitBtn;
 const defaultUsers = '{"users": [{"id": 1, "username": "testUser1", "password":"abc123", "name":"John Doe", "email":"john.doe@gmail.com"}, {"id": 2, "username": "testUser2", "password":"qwerty", "name":"Jane Smith", "email":"jane.smith@gmail.com"}]}';
+var usersMain;
 const users1 = JSON.parse(defaultUsers).users; // array
 var users2;
 
@@ -9,18 +9,18 @@ window.addEventListener("load", start, false);
 
 function start() {
 		form = document.getElementById("form");
-		name = document.getElementById("name");
+		name1 = document.getElementById("name");
 		email = document.getElementById("email");
   	username = document.getElementById("username");
   	password = document.getElementById("password");
   	password2 = document.getElementById("password2");
   	submitBtn = document.getElementById("submitBtn");
 
-  	
   	if (localStorage.getItem("addedUsers") === null) {
   		users2 = null;
   	} else {
-  		users2 = JSON.parse(localStorage.getItem("addedUsers")).users;
+  		usersMain = JSON.parse(localStorage.getItem("addedUsers"));
+  		users2 = usersMain.users;
   	}
 
   	form.addEventListener('submit', function (event) {
@@ -32,44 +32,63 @@ function start() {
           
           event.preventDefault();
           event.stopPropagation();
+        } else {
 
-          invalidDiv.className = "alert alert-danger";
-          invalidDiv.innerHTML = "The username or password is incorrect. Please try again."
-        } 
+        	/* store new user data into local storage*/
+        	if (users2 === null ) {
+        		
+        		// create object from user data
+        		const newObj = {users: [{id: 1, username: username.value, password: password.value, name: name1.value, email: email.value}]};
+        		const myJSON = JSON.stringify(newObj);
+        		localStorage.setItem("addedUsers", myJSON);
+        	} else {
+        		
+        		const newUser = {id: users2.length+1, username: username.value, password: password.value, name: name1.value, email: email.value}
+        		users2.push(newUser);
+        		const myJSON = JSON.stringify(usersMain);
+        		localStorage.setItem("addedUsers", myJSON);
+        	}
+        }
         
     }, false);
 }
 
 function validateForm() {
 
-	// need to validate username and password; set up database somewhere?
+	var allValid = true;
 
-	// check default user array
+	/* 1) check that username is unique */
 	for (var i = 0; i < users1.length; i++) {
-		if (users1[i].username == username.value) { // if username is found in users1
+			if (users1[i].username == username.value) { // if username is found in users1
+				username.classList.add("is-invalid"); 
+				username.addEventListener("change", function(){removeInvalid(username);}, false);
+				allValid = false;
+			} 
+		}
 
-			if (users1[i].password == password.value) { // check if password is correct
-				return true;
-			} else {
-				return false;
-			}
-		} 
-	}
 
-	// check added user array if not null
-	if (users2 !== null) {
+	if (users2 !== null) { // check addedUsers array too if exists
 		for (var i = 0; i < users2.length; i++) {
 			if (users2[i].username == username.value) { // if username is found in users2
-
-				if (users2[i].password == password.value) { // check if password is correct
-					return true;
-				} else {
-					return false;
-				}
+				username.classList.add("is-invalid"); 
+				username.addEventListener("change", function(){removeInvalid(username);}, false);
+				allValid = false;
 			} 
 		}
 	}
 
-	return false;
+	/* 2) check that password == password2 */
+	if (password.value != password2.value) {
+			password2.classList.add("is-invalid");
+			password2.addEventListener("change", function(){removeInvalid(password2);}, false);
+			allValid = false;
+	}
 	
+
+	return allValid;
+	
+}
+
+function removeInvalid(input) {
+		input.classList.remove("is-invalid");
 }
